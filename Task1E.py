b1e9da5bb98491a722a2a897b1d5cf5226b60c1c
Akeel-H <mam272@cam.ac.uk>
2023-02-04 04:34:00 +0000

    Exercise 1E

--- Task1E.py
+++ Task1E.py
@@ -2,4 +2,9 @@ from floodsystem import geo, stationdata
 
 stations = stationdata.build_station_list()
 
+river_stations = geo.stations_by_river(stations)
+
+stations = stationdata.build_station_list()
+
 print(geo.rivers_by_station_number(stations, 9))
+
